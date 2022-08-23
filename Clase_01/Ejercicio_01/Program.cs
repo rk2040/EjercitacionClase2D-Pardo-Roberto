41d5de3edce3ea89603082ada3cef2dc903ff8c7
rk2040 <rk2040@gmail.com>
2022-08-23 18:52:21 -0300

    ej 1 y 2

---
+++ Clase_01/Ejercicio_01/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+/*
+	Consigna
+	Ingresar 5 números por consola, guardándolos en una variable escalar. 
+	Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+*/
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            int numeroMaximo = int.MinValue;
+            int numeroMinimo = int.MaxValue;
+            int acumuladorDeNumeros = 0;
+            int cantidadDeNumeros = 0;
+            float promedioNumIngresados;
+
+            for (int i = 0; i < 5; i++)
+            {
+                numeros[i] = pidoNumeros();
+
+                acumuladorDeNumeros += numeros[i];
+                cantidadDeNumeros++;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Numero {i + 1}: {numeros[i]}");
+
+                numeroMaximo = mayorNumero(numeros[i], numeroMaximo);
+
+                numeroMinimo = menorNumero(numeros[i], numeroMinimo);
+            }
+            promedioNumIngresados = promedio(acumuladorDeNumeros, cantidadDeNumeros);
+
+            Console.WriteLine($"Mayor numero: {numeroMaximo} \nMenor numero: {numeroMinimo} \nEl promedio de los numeros es: {promedioNumIngresados}");
+
+
+
+        }
+
+        static int pidoNumeros()
+        {
+            int numero = 0;
+            string numeroString;
+            bool stringANumero;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numeroString = Console.ReadLine();
+            stringANumero = int.TryParse(numeroString, out numero);
+
+            while (stringANumero == false)
+            {
+                Console.WriteLine("Error, no es un numero. \nIngrese un numero: ");
+                numeroString = Console.ReadLine();
+                stringANumero = int.TryParse(numeroString, out numero);
+            } 
+
+            numero = int.Parse(numeroString);
+
+            return numero;
+        }
+
+        static int mayorNumero(int num1, int num2)
+        {
+            int mayor = 0;
+
+            if (num1 > num2)
+            {
+                mayor = num1;
+            }
+            else
+            {
+                mayor = num2;
+            }
+
+            return mayor;
+        }
+
+        static int menorNumero(int num1, int num2)
+        {
+            int menor = 0;
+
+            if (num1 < num2)
+            {
+                menor = num1;
+            }
+            else
+            {
+                menor = num2;
+            }
+
+            return menor;
+        }
+
+        static float promedio(int num1, int num2)
+        {
+            float resultado =  num1 / num2;
+
+            return resultado;
+        }
+    }
+}
---
+++ Clase_01/Ejercicio_02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/*
+     * Ingresar un número y mostrar el cuadrado y el cubo del mismo. 
+     * Se debe validar que el número sea mayor que cero,  contrario, 
+     * mostrar el mensaje: "ERROR. ¡Reingresar número!".
+ */
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            double cuadradoDeNumero;
+            double cuboDeNumero;
+            
+
+            numero = pidoNumeros();
+
+            cuadradoDeNumero = Math.Pow(numero, 2);
+
+            cuboDeNumero = Math.Pow(numero, 3);
+
+            Console.WriteLine($"El cuadrado de {numero} es: {cuadradoDeNumero}.\nEl cubo de {numero} es: {cuboDeNumero}");
+            
+        }
+
+        static int pidoNumeros()
+        {
+            int numero = 0;
+            string numeroString;
+            bool stringANumero;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numeroString = Console.ReadLine();
+            stringANumero = int.TryParse(numeroString, out numero);
+
+            while (stringANumero == false || numero <= 0)
+            {
+                Console.WriteLine("Error. Reingrese un numero: ");
+                numeroString = Console.ReadLine();
+                stringANumero = int.TryParse(numeroString, out numero);
+            } 
+
+            numero = int.Parse(numeroString);
+
+            return numero;
+        }
+    }
+}
