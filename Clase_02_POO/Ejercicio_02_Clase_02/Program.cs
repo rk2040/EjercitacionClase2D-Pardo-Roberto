0100794d1a664cc25be3bc2325e8ab11182c77f3
rk2040 <rk2040@gmail.com>
2022-08-25 23:50:51 -0300

    Eejrcicio 01 Clase 02

---
+++ Clase_02_POO/Ejercicio_02_Clase_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_02_Clase_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- Clase_02_POO/Ejercicio_02_Clase_02/obj/Debug/net5.0/Ejercicio_02_Clase_02.AssemblyInfo.cs
+++ Clase_02_POO/Ejercicio_02_Clase_02/obj/Debug/net5.0/Ejercicio_02_Clase_02.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio_01_Clase_02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio_02_Clase_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio_01_Clase_02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio_01_Clase_02")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio_02_Clase_02")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio_02_Clase_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
