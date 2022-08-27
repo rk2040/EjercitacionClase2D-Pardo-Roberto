13a4a48eb815d0ffb8c531545ae28f3133a6335b
rk2040 <rk2040@gmail.com>
2022-08-27 00:26:12 -0300

    Ejercicios

---
+++ Clase_01_ClasesYMetodosEstaticos/Ejercicio_01_Clase_01_ClasesYMetEstaticos/Validador.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Consigna
+    Realizar una clase llamada Validador que posea un método estático llamado Validar con la siguiente firma:
+
+    bool Validar(int valor, int min, int max)
+
+    valor: dato a validar.
+    min: mínimo valor incluido.
+    max: máximo valor incluido.
+    Pedir al usuario que ingrese 10 números enteros. Validar con el método desarrollado anteriormente que 
+    estén dentro del rango -100 y 100.
+
+    Terminado el ingreso mostrar el valor mínimo ingresado, valor máximo ingresado y el promedio.
+*/
+
+namespace Ejercicio_01_Clase_01_ClasesYMetEstaticos
+{
+    public class Validador
+    {
+        int valor;
+        int valorMinimo;
+        int valorMaximo;
+
+        public Validador(int valor, int valorMinimo, int valorMaximo)
+        {
+            this.valor = valor;
+            this.valorMinimo = valorMinimo;
+            this.valorMaximo = valorMaximo;
+        }
+
+        public static bool Validar(int valor, int min, int max)
+        {
+            bool esNumero = false;
+
+            if (valor >= min && valor <= max) esNumero = true;
+
+            return esNumero;
+        }
+
+        public static int EsNumero(string numeroString)
+        {
+            bool esNumero = false;
+            int numero = 0;
+
+
+            do
+            {
+               // Console.WriteLine("Ingrese un numero: ");
+                numeroString = Console.ReadLine();
+                esNumero = int.TryParse(numeroString, out numero);
+
+                if(!esNumero) Console.WriteLine("Error, no es un numero.");
+            } while (esNumero == false) ;
+                return numero;
+        }
+    }
+
+
+}
---
+++ Clase_01_ClasesYMetodosEstaticos/Ejercicio_02_Clase_01_ClasesYMetEstaticos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_02_Clase_01_ClasesYMetEstaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
